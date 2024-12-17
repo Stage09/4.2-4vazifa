bcb614f644489a530ad2704043e60f23f8d22fa1
Stage777999 <ikingib1979@gmail.com>
2024-12-17 15:53:32 +0500

    finished

---
+++ main.js
@@ -0,0 +1,63 @@
+// 1-masala   || https://carbon.now.sh/fLCQTYV17h7i9Bfa88x8 ||
+
+// for (let i = 0; i <= 10; i++) {
+//     if (i === 0) {
+//         console.log(i + " - nol son");
+//     } else if (i % 2 === 0) {
+//         console.log(i + " - juft son");
+//     } else {
+//         console.log(i + " - toq son");
+//     }
+// }
+
+
+// 2-masala  || https://carbon.now.sh/8kHlNAxBP6hi9gpbbNoE ||
+
+// let sum = 0;
+
+// for (let i = 1; i <= 10; i++) {
+//     sum += i;
+// }
+
+// console.log("Yiq'indi: " + sum);
+
+// 3-masala || https://carbon.now.sh/p8y0Ql37cEHvXeWJiPle ||
+
+// let n = parseInt(prompt("Nechta yulduzcha chiqarmoqchisiz? "));
+
+// for (let i = 1; i <= n; i++) {
+//     let stars = '';
+//     for (let j = 1; j <= i; j++) {
+//         stars += '*';
+//     }
+//     console.log(stars);
+// }
+
+// 4-masala
+
+// let num1 = prompt("Birinchi raqamni kiriting:")
+// let num2 = prompt("Ikkinchi raqamni kiriting:")
+// let num3 = prompt("Uchinchi raqamni kiriting:")
+
+// if (num1 <= num2 && num1 <= num3) {
+//     console.log("Eng kichik son: " + num1);
+// } else if (num2 <= num1 && num2 <= num3) {
+//     console.log("Eng kichik son: " + num2);
+// } else {
+//     console.log("Eng kichik son: " + num3);
+// }
+
+
+// 5-masala
+
+// let num1 = prompt("Birinchi raqamni kiriting:")
+// let num2 = prompt("Ikkinchi raqamni kiriting:")
+// let num3 = prompt("Uchinchi raqamni kiriting:")
+
+// if ((num1 >= num2 && num1 <= num3) || (num1 <= num2 && num1 >= num3)) {
+//     console.log("O'rta raqam: " + num1);
+// } else if ((num2 >= num1 && num2 <= num3) || (num2 <= num1 && num2 >= num3)) {
+//     console.log("O'rta raqam: " + num2);
+// } else {
+//     console.log("O'rta raqam: " + num3);
+// }
